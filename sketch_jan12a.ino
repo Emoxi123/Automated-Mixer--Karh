e141da2d559b1cd1aea7d046084b64417e6c0b8c
Emoxi123 <emil.docev2000@gmail.com>
2018-01-12 10:12:25 +0200

    Add files via upload

---
+++ sketch_jan12a.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(3,OUTPUT);
+  digitalWrite(3,LOW);
+
+  pinMode(4,OUTPUT);
+  digitalWrite(4,LOW);
+
+}
+//da go zapoim; scl i sda kum arduinoto za danni
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(4,HIGH);
+ delay(50);
+ digitalWrite(4,LOW);
+ delay(50); 
+}
